6065848d68623f6f1bc92a1fdda2b9efbed84085
Slaiter <Slaiter89@mail.ru>
2018-03-26 23:17:53 +0300

    Лекция 2.6. Заменяем наследование на делегирование и создаём двухуровневую архитектуру

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
