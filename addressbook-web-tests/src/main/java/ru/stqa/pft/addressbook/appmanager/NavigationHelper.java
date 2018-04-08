c8b3884b72ff8a37c08c6bf86e3fbf4cd230a005
Slaiter <Slaiter89@mail.ru>
2018-04-08 19:59:05 +0300

    Лекция 3.9. "Ленивые" действия

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,10 +10,22 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
+        if (isElemetPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElemetPresent(By.name("New"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoContactPage() {
         wd.findElement(By.linkText("add new")).click();
+    }
+
+    public void gotoHomePage() {
+        if (isElemetPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("add new"));
     }
 }
