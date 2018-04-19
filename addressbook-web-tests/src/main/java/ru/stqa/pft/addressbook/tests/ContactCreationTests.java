3c46734d18b4d8bbadeadf4b8c8793bdf2080253
Slaiter <Slaiter89@mail.ru>
2018-04-19 23:33:08 +0300

    Лекция 6.1. Пути к файлам и директориям

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -12,10 +14,11 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         Contacts before = app.contact().all();
+        File photo = new File("src/test/resources/stru.png");
         ContactData contact = new ContactData()
                 .withFirstname("bob").withLastname("petrov").withHomePhone("111")
-                .withMobilePhone("222").withWorkPhone("333").withEmail("qiwi77@mail.ru")
-                .withGroup("test1").withAddress("testaddress").withEmail("email")
+                .withMobilePhone("222").withWorkPhone("333").withEmail("qiwi77@mail.ru").withGroup("test1")
+                .withPhoto(photo).withAddress("testaddress").withEmail("email")
                 .withEmail2("email2").withEmail3("email3");
         app.contact().create(contact);
         Contacts after = app.contact().all();
