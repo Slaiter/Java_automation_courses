c0ef3b9a25dca6863a2ac16d928a685768d184d4
Slaiter <Slaiter89@mail.ru>
2018-04-22 22:53:21 +0300

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,38 +3,79 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table(name = "addressbook")
 @XStreamAlias("contact")
 public class ContactData {
+
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "firstname")
     private String firstname;
+
     @Expose
+    @Column(name = "lastname")
     private String lastname;
-    private String homePhone;
-    private String mobilePhone;
-    private String workPhone;
+
     @Expose
-    private String allPhones;
-    private String address;
-    private String email;
-    private String email2;
-    private String email3;
-    @Expose
-    private String allEmails;
-    @Expose
+    @Transient
     private String group;
-    private File photo;
+
+    @Column(name = "home")
+    @Type(type = "text")
+    private String homePhone;
+
+    @Column(name = "mobile")
+    @Type(type = "text")
+    private String mobilePhone;
+
+    @Column(name = "work")
+    @Type(type = "text")
+    private String workPhone;
+
+    @Expose
+    @Transient
+    private String allPhones;
+
+    @Column(name = "address")
+    @Type(type = "text")
+    private String address;
+
+    @Column(name = "email")
+    @Type(type = "text")
+    private String email;
+
+    @Column(name = "email2")
+    @Type(type = "text")
+    private String email2;
+
+    @Column(name = "email3")
+    @Type(type = "text")
+    private String email3;
+
+    @Expose
+    @Transient
+    private String allEmails;
+
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
