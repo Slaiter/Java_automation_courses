b86233f1013fbfb78531911ece0cd4ff887e0fd9
Slaiter <Slaiter89@mail.ru>
2018-04-22 13:33:42 +0300

    Лекция 6.8. Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -38,15 +38,6 @@ public class GroupDataGenerator {
         generator.run();
     }
 
-    private static void saveAsCsv(List<GroupData> groups, File file) throws IOException {
-        System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (GroupData group : groups) {
-            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
-        }
-        writer.close();
-    }
-
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
         if (format.equals("csv")) {
@@ -63,9 +54,9 @@ public class GroupDataGenerator {
     private void saveAsJson(List<GroupData> groups, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(groups);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -73,9 +64,18 @@ public class GroupDataGenerator {
         xstream.processAnnotations(GroupData.class);
         xstream.alias("group", GroupData.class);
         String xml = xstream.toXML(groups);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
+    }
+
+    private static void saveAsCsv(List<GroupData> groups, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        try (Writer writer = new FileWriter(file)) {
+            for (GroupData group : groups) {
+                writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+            }
+        }
     }
 
     private List<GroupData> generateGroups(int count) {
