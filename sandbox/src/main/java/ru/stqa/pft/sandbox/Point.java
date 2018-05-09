f7e6e0598e88118aa00e00d8e5d32f1b8d6e331f
Slaiter <Slaiter89@mail.ru>
2018-05-09 04:03:06 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы (bug fix)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,29 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-    double x;
-    double y;
+    public double x;
+    public double y;
 
-    Point(double x, double y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    double getDistance(Point a, Point b) {
-        return Math.sqrt(Math.pow(x - a.x, 2) + Math.pow(y - b.y, 2));
-    }
-
-    boolean isSame(Point a) {
-        if (x == a.x && y == a.y) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    double getRadius() {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 0);
-        return this.getDistance(a, b);
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow((this.x - p.x), 2) + Math.pow((this.y - p.y), 2));
     }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointTest.java
@@ -3,25 +3,17 @@ package ru.stqa.pft.sandbox;
 public class PointTest {
 
     public static void main(String[] args) {
+        Point firstPoint = new Point(5.0, 5.0);
+        Point secondPoint = new Point(10.0, 10.0);
+        displayOnScreen(firstPoint.distance(secondPoint));
+        displayOnScreen(secondPoint.distance(firstPoint));
+    }
 
-        System.out.println("Задайте координаты первой точки на плоскости: ");
-        Point a = new Point(1, 1);
+    public static void displayOnScreen (double distance) {
+        System.out.println("Расстояние между двумя случайными точками равно = " + distance);
+    }
 
-        System.out.println("Задайте координаты второй точки на плоскости: ");
-        Point b = new Point(5, 5);
-
-        if (a.isSame(b)) {
-            System.out.println("Точки совпадают!");
-        } else {
-            System.out.println("Расстояние между точками: " + b.getDistance(a, b));
-        }
-
-        if (a.getRadius() < b.getRadius()) {
-            System.out.println("К началу координат ближе первая точка!");
-        } else if (a.getRadius() == b.getRadius()) {
-            System.out.println("Точки равноудалены от начала координат!");
-        } else {
-            System.out.println("К началу координат ближе вторая точка!");
-        }
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 }
